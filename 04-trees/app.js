cfb979a4df76be6cd142f4fac12ba51ef104f7a5
arroyostack <arroyostack@gmail.com>
2023-10-24 09:25:13 +0200

    Continue in balance vs unbalance.

---
+++ 04-trees/app.js
@@ -0,0 +1,14 @@
+// Level 0: 2^0 = 0;
+// Level 1: 2^1 = 2;
+// Level 2: 2^2 = 4;
+// Level 3: 2^3 = 8;
+
+export const trees = ( () => {
+
+    const binaryTreeNode = ( value ) => {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    };
+
+} )();
